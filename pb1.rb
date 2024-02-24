ccf7817968fc4390bf79e1e88c93e3a1cab98659
Nicholas Bradley <bradleyhayden6932@gmail.com>
2024-02-24 14:03:13 -0600

    problem 1

---
+++ pb1.rb
@@ -0,0 +1,20 @@
+RSpec.describe User do
+    describe "#log_in" do
+        it "logs the user in" do
+            user.log_in
+            expect(user.logged_in).to be(true)
+        end
+    end
+    describe "#log_out" do
+        it "logs the user out" do
+            user.log_in
+            user.log_out
+            expect(user.logged_in).to be(false)
+        end
+    end
+    describe "#username" do
+        it "returns the correct username" do
+          expect(user.username).to eq(username)
+        end
+    end
+end
